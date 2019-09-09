4acfc3e90ba6a88ffa64fe82ca21da3833c035c3
Arnaud Durand <arnaud.durand@unifr.ch>
2019-09-09 17:37:44 +0200

    Move GPS handler to ble2mqtt

--- main/ble2mqtt.c
+++ main/ble2mqtt.c
@@ -150,12 +150,61 @@ static void ota_unsubscribe(void)
     mqtt_unsubscribe("BLE2MQTT/OTA/Config");
 }
 
+/* GPS functions */
+/**
+ * @brief GPS Event Handler
+ *
+ * @param event_handler_arg handler specific arguments
+ * @param event_base event base, here is fixed to ESP_NMEA_EVENT
+ * @param event_id event id
+ * @param event_data event specific arguments
+ */
+static void gps_on_event(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
+{
+    gps_t *gps = NULL;
+    switch (event_id) {
+    case GPS_UPDATE:
+        gps = (gps_t *)event_data;
+        /* print information parsed from GPS statements */
+        ESP_LOGI(TAG, "%d/%d/%d %d:%d:%d => \r\n"
+                 "\t\t\t\t\t\tlatitude   = %.05f°N\r\n"
+                 "\t\t\t\t\t\tlongtitude = %.05f°E\r\n"
+                 "\t\t\t\t\t\taltitude   = %.02fm\r\n"
+                 "\t\t\t\t\t\tspeed      = %fm/s",
+                 gps->date.year + YEAR_BASE, gps->date.month, gps->date.day,
+                 gps->tim.hour + TIME_ZONE, gps->tim.minute, gps->tim.second,
+                 gps->latitude, gps->longitude, gps->altitude, gps->speed);
+        if (gps->valid)
+        {
+            char topic[MAX_TOPIC_LEN];
+            char payload[16];
+
+            sprintf(payload, "%.05f,%.05f", gps->latitude, gps->longitude);
+            snprintf(topic, MAX_TOPIC_LEN, "%s/Position", device_name_get());
+            mqtt_publish(topic, (uint8_t *)payload, strlen(payload),
+            config_mqtt_qos_get(), config_mqtt_retained_get());
+
+            sprintf(payload, "%f", gps->speed);
+            snprintf(topic, MAX_TOPIC_LEN, "%s/Speed", device_name_get());
+            mqtt_publish(topic, (uint8_t *)payload, strlen(payload),
+            config_mqtt_qos_get(), config_mqtt_retained_get());
+        }
+        break;
+    case GPS_UNKNOWN:
+        /* print unknown statements */
+        ESP_LOGW(TAG, "Unknown statement:%s", (char *)event_data);
+        break;
+    default:
+        break;
+    }
+}
+
 static void cleanup(void)
 {
     ble_disconnect_all();
     ble_scan_stop();
     ota_unsubscribe();
-    gps_stop();
+    gps_stop(gps_on_event);
 }
 
 /* Wi-Fi callback functions */
@@ -186,7 +235,7 @@ static void mqtt_on_connected(void)
     self_publish();
     ota_subscribe();
     ble_scan_start();
-    gps_start();
+    gps_start(gps_on_event);
 }
 
 static void mqtt_on_disconnected(void)
--- main/gps.h
+++ main/gps.h
@@ -1,8 +1,14 @@
 #ifndef GPS_H
 #define GPS_H
 
-int gps_start(void);
-int gps_stop(void);
+#include "nmea_parser.h"
+
+#define GPS_TX_PIN (12)
+#define TIME_ZONE (+2)   //Bern Time
+#define YEAR_BASE (2000) //date in GPS starts from 2000
+
+int gps_start(void (*gps_event_handler));
+int gps_stop(void (*gps_event_handler));
 int gps_initialize(void);
 
 #endif
