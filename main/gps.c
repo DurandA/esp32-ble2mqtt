3cf9e9ff9e889ca9b08610d1da13a28816cfcab0
Arnaud Durand <arnaud.durand@unifr.ch>
2019-09-09 17:37:44 +0200

    Move GPS handler to ble2mqtt

--- main/gps.c
+++ main/gps.c
@@ -1,49 +1,14 @@
-#include <esp_log.h>
+#include "gps.h"
 #include "nmea_parser.h"
+#include "wifi.h"
+#include <esp_log.h>
 
 /* Constants */
-static const char *TAG = "MQTT";
-#define GPS_TX_PIN (18)
-#define TIME_ZONE (+8)   //Beijing Time
-#define YEAR_BASE (2000) //date in GPS starts from 2000
+static const char *TAG = "GPS";
 
 static nmea_parser_handle_t nmea_hdl;
 
-/**
- * @brief GPS Event Handler
- *
- * @param event_handler_arg handler specific arguments
- * @param event_base event base, here is fixed to ESP_NMEA_EVENT
- * @param event_id event id
- * @param event_data event specific arguments
- */
-static void gps_event_handler(void *event_handler_arg, esp_event_base_t event_base, int32_t event_id, void *event_data)
-{
-    gps_t *gps = NULL;
-    switch (event_id) {
-    case GPS_UPDATE:
-        gps = (gps_t *)event_data;
-        /* print information parsed from GPS statements */
-        ESP_LOGI(TAG, "%d/%d/%d %d:%d:%d => \r\n"
-                 "\t\t\t\t\t\tlatitude   = %.05f°N\r\n"
-                 "\t\t\t\t\t\tlongtitude = %.05f°E\r\n"
-                 "\t\t\t\t\t\taltitude   = %.02fm\r\n"
-                 "\t\t\t\t\t\tspeed      = %fm/s",
-                 gps->date.year + YEAR_BASE, gps->date.month, gps->date.day,
-                 gps->tim.hour + TIME_ZONE, gps->tim.minute, gps->tim.second,
-                 gps->latitude, gps->longitude, gps->altitude, gps->speed);
-        break;
-    case GPS_UNKNOWN:
-        /* print unknown statements */
-        ESP_LOGW(TAG, "Unknown statement:%s", (char *)event_data);
-        break;
-    default:
-        break;
-    }
-}
-
-
-int gps_start(void)
+int gps_start(void (*gps_event_handler))
 {
     /* register event handler for NMEA parser library */
     nmea_parser_add_handler(nmea_hdl, gps_event_handler, NULL);
@@ -51,7 +16,7 @@ int gps_start(void)
     return 0;
 }
 
-int gps_stop(void)
+int gps_stop(void (*gps_event_handler))
 {
 	/* unregister event handler */
     nmea_parser_remove_handler(nmea_hdl, gps_event_handler);
